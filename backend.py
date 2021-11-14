bbc03ece512b0adb6287354b4fb9b48ff7ef80f7
williamrmathias <williamrmathias45@gmail.com>
2021-11-14 12:18:36 -0500

    very basic website

--- backend.py
+++ backend.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Welcome to Abstract Art Website!!!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.debug = True
